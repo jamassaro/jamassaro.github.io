9158988eed41956e361feb227b5c0a0f5d3cbd55
jamassaro <67125285+jamassaro@users.noreply.github.com>
2023-10-29 23:28:34 -0400

    implement routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home/Home"
+import ProjectPage from './pages/projects/ProjectPage'
+import LayOut from './components/LayOut/LayOut'
 
 
 function App() {
   return (
-   <Home />
+    <Router>
+      <Routes>
+          <Route 
+            path='/' 
+            element={
+              <LayOut>  
+                <Home /> 
+              </LayOut>
+              } 
+            exact
+          />
+          <Route path='/projects/:name' element={<LayOut><ProjectPage/></LayOut>} exact/>
+ 
+      </Routes>
+    </Router> 
   )
 }
 
