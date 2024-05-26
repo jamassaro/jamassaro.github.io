946f469a32c2f1c8d6e30f7050653944413433b1
jamassaro <67125285+jamassaro@users.noreply.github.com>
2024-05-26 12:49:02 -0400

    add burger menu and fix responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './navbar.css'
-import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const navigate = useNavigate()
@@ -19,17 +19,27 @@ const Navbar = () => {
   }
 
   return (
+    <>
+    <div className='navbar-container-mobile'>
+    <Link to='/'><h1>My Portfolio</h1></Link>
+    <div className='hamburger-menu'>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    </div>
     <div className='nav-container'>
       <div className='logo-wrapper'>
         <h3 className='name-logo'>JoseMassaro._</h3>
       </div>
       <div className='navigation-wrapper'>
         <Link to='/'>home</Link>
-       <h5 onClick={() =>handleNavigation('my-expertise')}>expertise</h5>
-       <h5 onClick={() =>handleNavigation('my-work')}>work</h5>
-       <h5 onClick={() =>handleNavigation('/contact')}>contact</h5>
+        <h5 onClick={() => handleNavigation('my-expertise')}>expertise</h5>
+        <h5 onClick={() => handleNavigation('my-work')}>work</h5>
+        <h5 onClick={() => handleNavigation('/contact')}>contact</h5>
       </div>
     </div>
+    </>
   )
 }
 
